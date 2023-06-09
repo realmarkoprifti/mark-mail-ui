f0b2a67271f7fab0fcbdb0a88e879782df6ff353
mPenTests <marko.prifti4@gmail.com>
2023-06-09 22:18:10 +0200

    Final Commit

--- src/components/api_requests.js
+++ src/components/api_requests.js
@@ -2,7 +2,7 @@ import axios from 'axios'
 import { formToJSON } from 'axios'
 
 
-const url = "http://127.0.0.1:8000/api"
+const url = "https://markmail-production.up.railway.app/api"
 
 export async function login(dataform, recaptcha_token) {
     const form = formToJSON(dataform)
